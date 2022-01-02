8d867663a1f1267d910a2197f06ed3dbcbdde2a1
Rahul <58558341+rahulps1000@users.noreply.github.com>
2022-01-02 20:48:25 +0530

    Update inline_query_result_cached_document.py

--- pyrogram/types/inline_mode/inline_query_result_cached_document.py
+++ pyrogram/types/inline_mode/inline_query_result_cached_document.py
@@ -95,7 +95,7 @@ class InlineQueryResultCachedDocument(InlineQueryResult):
                 await self.input_message_content.write(client, self.reply_markup)
                 if self.input_message_content
                 else raw.types.InputBotInlineMessageMediaAuto(
-                    reply_markup=self.reply_markup.write(client) if self.reply_markup else None,
+                    reply_markup=await self.reply_markup.write(client) if self.reply_markup else None,
                     **await(Parser(None)).parse(self.caption, self.parse_mode)
                 )
             )
